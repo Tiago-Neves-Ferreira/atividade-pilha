7b0efeb212918a69028b6dc3e18ee2db09351489
Tiago Neves <94819256+Tiago-Neves-Ferreira@users.noreply.github.com>
2022-04-09 17:42:29 -0300

    Add files via upload

---
+++ javapallet/src/javapallet/Pallet.java
@@ -0,0 +1,28 @@
+package javapallet;
+
+public class Pallet {
+    private String produto;
+    private int qtd;
+
+    public String getProduto() {
+        return produto;
+    }
+    public void setProduto(String produto) {
+        this.produto = produto;
+    }
+    public int getQtd() {
+        return qtd;
+    }
+    public void setQtd(int qtd) {
+        this.qtd = qtd;
+    }
+    
+    
+   @Override
+public String toString()
+{
+    return getProduto()+ " : " + getQtd();
+    
+} 
+}
+
